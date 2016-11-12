ee6cf0aea95a474065db200f11b6ab77a53e59b2
李东旭 <lidongxu@lidongxus-iMac.local>
2016-11-12 18:03:54 +0800

    2016-11-12 lidongxu 开启健康监测接口.

--- spring-cloud-demo/spring-cloud/spring-cloud-base/src/main/java/com/demo/base/SpringCloudBaseMain.java
+++ spring-cloud-demo/spring-cloud/spring-cloud-base/src/main/java/com/demo/base/SpringCloudBaseMain.java
@@ -5,10 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableEurekaClient
+@EnableFeignClients
 public class SpringCloudBaseMain {
 	
 	static Logger log = LoggerFactory.getLogger(SpringCloudBaseMain.class);
